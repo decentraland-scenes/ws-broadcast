7ae14d0e87f9e8bb262f5271103c33a79a894779
Kyllian LE BORGNE ROEPRCH <me@hprivakos.net>
2020-07-07 22:47:28 +0200

    feat: Websocket server

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple broadcasting websocket server
+
+This project serve as an example on how to deploy a server with secure websocket.
+It opens a websocket server on port 8080 which can then be proxied via nginx to add TLS certificate
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import * as WebSocket from "ws";
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+interface customWs extends WebSocket {
+  room: string;
+}
+
+wss.on("connection", (clientWs, request) => {
+  const ws = clientWs as customWs;
+  ws.room = request.url || "";
+
+  ws.on("message", function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      const cWs = client as customWs;
+
+      if (cWs.readyState === WebSocket.OPEN && cWs.room === ws.room) {
+        cWs.send(data);
+      }
+    });
+  });
+});
